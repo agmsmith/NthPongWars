9b0ebc4452a6e113455cc0c70d247ce218120e5d
Alexander G. M. Smith <agmsmith@ncf.ca>
2024-04-21 18:12:34 -0400

    WIP - Bit shifts better than dividing.

--- Hello/main.c
+++ Hello/main.c
@@ -2,7 +2,8 @@
  * AGMS20240416 Add some code to see how the compiler handles 16 bit fixed
  * point fractions.
  *
- * Compile for NABU + RetroNet Cloud CP/M (generates a hard disk image) with:
+ * Compile for NABU + RetroNet Cloud CP/M (generates a hard disk image and a
+ * usable .COM file, --math32 for floating point) with:
  * zcc +cpm -subtype=naburn --list -gen-map-file -gen-symbol-file -create-app -compiler=sdcc --opt-code-speed --math32 main.c -o "TestNumb"
 */
 
@@ -12,29 +13,44 @@
 #include <stdio.h>
 
 
-void FixedPointTest(void)
+signed char FixedPoint16Test(signed short NumA, signed short NumB)
 {
-  signed short NumA = 42.1 * 64;
-  signed short NumB = 17.5 * 64;
-  signed short NumC = 0;
-  signed short NumD = 0;
-  signed short NumE = 0;
+  signed short NumC;
 
-  printf ("And now a test of fixed point math speed by AGMS20240416.\n");
   NumC = NumA + NumB;
-  printf ("Adding %f plus %f yields %f.\n",
-    NumA / 64.0, NumB / 64.0, NumC / 64.0);
-  NumD = NumC / 64;
-  NumE = NumC % 64;
-  printf ("Or converting using integer math, result is %d and %d/64.\n",
-    NumD, NumE);
+  return (signed char) (NumC >> 6);
 }
 
 
-int main(int argc, char **argv)
+signed short int FixedPoint32Test(signed long NumA, signed long NumB)
 {
+  signed long NumC;
+
+  NumC = NumA + NumB;
+  return (signed short int) (NumC >> 16);
+}
+
+
+int main(void)
+{
+  int i;
+  signed short Sum16 = 0;
+  signed long Sum32 = 0;
+
   printf("Hello World!\n");
   printf ("Test for basic compatibility by AGMS20240304.\n");
-  FixedPointTest();
-  return 0;
+
+  for (i = 0; i < 30000; i++)
+  {
+    Sum16 += FixedPoint16Test(42.1 * 64, 17.5 * 64);
+  }
+  printf ("Finished fixed point test, sum is %d.\n", Sum16);
+
+
+  for (i = 0; i < 30000; i++)
+  {
+    Sum32 += FixedPoint16Test(42.1 * 65536, 17.5 * 65536);
+  }
+  printf ("Finished fixed point test, sum is %ld.\n", Sum32);
+return 0;
 }
